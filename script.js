05d7c69b27960b669f94160ae1d524c1449776d7
magda-u <magdalena.urzon@gmail.com>
2019-03-13 23:30:49 +0100

    add bin function

--- script.js
+++ script.js
@@ -6,37 +6,42 @@ const form = document.querySelector('form');
 let deleteX = document.querySelector('.fa-times');
 
 
+
 //constructor
 function book(title, author) {
     this.title = title;
     this.author = author;
+    //date
+    const time = new Date;
+    const day = time.getDate();
+    const month = time.getMonth();
+    const year = time.getFullYear();
+    this.date = `${day}/${month}/${year}`;
 }
 
 // add new element of array to read_table
 
 function addTab() {
     let i = myLibrary.length;
-    const time = new Date;
-    const day = time.getDate();
-    const month = time.getMonth();
-    const year = time.getFullYear();
+    let divMain = document.createElement('div');
     let div1 = document.createElement('div');
     let div2 = document.createElement('div');
     let div3 = document.createElement('div');
     let div4 = document.createElement('div');
     let div5 = document.createElement('div');
-    let div6 = document.createElement('div');
-    tab.appendChild(div1);
-    tab.appendChild(div2);
-    tab.appendChild(div3);
-    tab.appendChild(div4);
-    tab.appendChild(div5);
-    tab.appendChild(div6);
-    div1.innerHTML = i;
-    div2.innerHTML = myLibrary[i - 1].title;
-    div3.innerHTML = myLibrary[i - 1].author;
-    div4.innerHTML = `${day}/${month}/${year}`;
-    div6.innerHTML = 'x';
+    tab.appendChild(divMain);
+    divMain.classList.add('read_table_row');
+    divMain.appendChild(div1);
+    divMain.appendChild(div2);
+    divMain.appendChild(div3);
+    divMain.appendChild(div4);
+    divMain.appendChild(div5);
+    div1.innerHTML = myLibrary[i - 1].title;
+    div2.innerHTML = myLibrary[i - 1].author;
+    div3.innerHTML = myLibrary[i - 1].date;
+    div5.classList.add('bin')
+    const bins = document.querySelectorAll('.bin');
+    bins.forEach(bin => bin.addEventListener('click', removeBook))
 }
 
 //show form to add new book
@@ -65,26 +70,33 @@ function addBook(e) {
     }
 }
 
+//remove book from table
+function removeBook(e) {
+    e.target.parentNode.remove();
+}
+
 
 
 btnNew.addEventListener('click', showAdd);
 btnAdd.addEventListener('click', addBook);
 deleteX.addEventListener('click', hideAdd);
 
+
 //add assessment of book
-let starOne = document.querySelector('.star1');
-let starTwo = document.querySelector('.star1');
-let starThree = document.querySelector('.star1');
-let starFour = document.querySelector('.star1');
-let starFive = document.querySelector('.star1');
+// let starOne = document.querySelector('.star1');
+// let starTwo = document.querySelector('.star1');
+// let starThree = document.querySelector('.star1');
+// let starFour = document.querySelector('.star1');
+// let starFive = document.querySelector('.star1');
 
-let stars = [
-    ...document.querySelectorAll('.fa-star')
-];
+// let stars = [
+//     ...document.querySelectorAll('.fa-star')
+// ];
 
-function addStar() {
-    //     let number =
-    //         this.classList.add('activeStar');
-}
+// function addStar() {
+//     //     let number =
+//     //         this.classList.add('activeStar');
+//     //indexOf?
+// }
 
-stars.forEach(star => star.addEventListener('click', addStar))
+// stars.forEach(star => star.addEventListener('click', addStar))
